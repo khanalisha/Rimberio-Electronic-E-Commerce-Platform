6213b8f2ce63d0e7963bee6a88c2f71ff69db12e
khanalisha <123863034+khanalisha@users.noreply.github.com>
2023-04-01 17:32:18 +0530

    day-5

---
+++ product.js
@@ -0,0 +1,397 @@
+const all_produt = [
+
+    {
+        "id": 0,
+        "image": "https://www.reliancedigital.in/medias/Noise-SENSE-BLK-HeadphonesandHeadsets-492579170-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w5OTczfGltYWdlL2pwZWd8aW1hZ2VzL2g1Ni9oZTYvOTc4MDE5NzI2MTM0Mi5qcGd8NTlkNWFiOTE3MzZmNzZhM2YyYTUyMzQ2NTBiN2M4Yzc4NzQ5NzhiOTM0MGI5Njc4YzBmYTE1MmFiYzNmNDcxMg",
+        "title": "Noise Sense Bluetooth Wireless in Ear, Neckband Earphones with Fast Charging, Up to 25H Playback, Powerful Bass, with mic for Clear Calls, Type C Port, IPX5, Voice Assistant (Jet Black)",
+        "brand": "BOAT",
+        "rating": 4.9,
+        "offerprice": 899.0,
+        "mrp": 2499.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 1,
+        "image": "https://www.reliancedigital.in/medias/boAt-Stone-Grenade-Speaker-491893336-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0OTYxM3xpbWFnZS9wbmd8aW1hZ2VzL2hhOC9oMjkvOTc3NTI3NjI2MTQwNi5wbmd8OGU2ZDEwNGRkNjBmNWJiYWNlMGFjNDkxYjQ4Y2Q2YmQ5ODkxOWZjNmE2NjhlMzAyNzA0YWU1MDJmZDBiOWFmOA",
+        "title": "boAt Stone Grenade 5W Portable Wireless Speaker With Multiple Connectivity Modes, Up to 7H Playback, IPX6 Water and Splash Resistance and Rugged Shock Resistant Design, Black",
+        "brand": "BOAT",
+        "rating": 4.5,
+        "offerprice": 899.0,
+        "mrp": 3999.0,
+        "category": "Speaker"
+    },
+    {
+        "id": 2,
+        "image": "https://www.reliancedigital.in/medias/boAt-Airdopes-138-Airdopes-491973383-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0NTY1fGltYWdlL2pwZWd8aW1hZ2VzL2gyMS9oNDAvOTkwNDQ4NjY3ODU1OC5qcGd8OGMyZjU0OGZmN2RmMDg5ZGQwNzU1ZjAyMmE1YWMxNWQ5NDU4MzdkZmRmNTQ0ZDI3N2I4Nzg5YzJmMjgwMDA3Mw",
+        "title": "boAt Airdopes 138 Twin Wireless Earbuds with IWP Technology, Bluetooth V5.0, Immersive Audio, Up to 15H Total Playback, Instant Voice Assistant and Type-C Charging, Steel Blue",
+        "brand": "BOAT",
+        "rating": 4.7,
+        "offerprice": 1199.0,
+        "mrp": 2700.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 3,
+        "image": "https://www.reliancedigital.in/medias/Hammer-Sting-3-0-Earphone-492796748-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDY2MnxpbWFnZS9qcGVnfGltYWdlcy9oZjIvaGUwLzk4MTU4MjIxNzIxOTAuanBnfGQ2NjY3MmI2ODYwNzllM2EzODZmM2JmZmUzMDM4MDIyY2M5YzVhMzA3ZGU4M2FlNTdlN2FkZjA5N2I1ZWJkODE",
+        "title": "Hammer Sting 3 Bluetooth Wireless Neckband Earphone with Sweat-proof and Deep Bass, Black Red with 1 year warranty",
+        "brand": "HAMMER",
+        "rating": 4.4,
+        "offerprice": 1700.0,
+        "mrp": 2300.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 4,
+        "image": "https://www.reliancedigital.in/medias/Reconnect-BE01802-Headphone-Earphone-492796670-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNzE3NnxpbWFnZS9qcGVnfGltYWdlcy9oZGUvaDQ1Lzk4OTU5MzAxNjczMjYuanBnfDhhMzU0OGY2YWUxYjAxY2Q5NzNkZjVkZWE1NjY2ZjY2ZWRlZmU0ZGY4NjE1NDliYTQ2OGI2MTIwODYxMmVlOWE",
+        "title": "Reconnect Dank Bluetooth Earphone, with IPx4, water resistance, 16Hrs Battery , Multi point connection (Black) with 1 year warranty",
+        "brand": "NOTHING1",
+        "rating": 4.9,
+        "offerprice": 3700.0,
+        "mrp": 5900.0,
+        "category": "Earphone"
+    },
+
+    {
+        "id": 5,
+        "image": "https://www.reliancedigital.in/medias/pTron-Basspods-P181-Bluetooth-Earphones-492796965-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w0MjMyM3xpbWFnZS9qcGVnfGltYWdlcy9oYWIvaDlmLzk4MjU4OTQ4MjYwMTQuanBnfDFiNDA1MjIwOGNkMjM4YmVmYThiMjY2OWEyMzg3OWRmNDQ3MDBhYzMwYjcxOTMyZWEwNmRhMDQyMWMyMDZmZTk",
+        "title": "pTron Basspods P181 Bluetooth 5.1 Wireless Headphones, 32Hrs Total Playtime, Immersive Stereo Sound, Stereo Calls, Snug-fit TWS Earbuds, Touch Controls, Voice Assistance, Type-C Fast Charging & IPX4 (Black)",
+        "brand": "pTRON",
+        "rating": 4.7,
+        "offerprice": 699.0,
+        "mrp": 2999.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 6,
+        "image": "https://www.reliancedigital.in/medias/boAt-Rockerz-255-Pro-Airdopes-492913061-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3NTAyfGltYWdlL2pwZWd8aW1hZ2VzL2g1Zi9oZDkvOTkwNDUxNTUxNDM5OC5qcGd8MWRkMDYzN2ZjNjEzNDlkMTc0MjE5ZjI1ODk0OWNiZjJlNDhjZDA3MjNjYzYxNGNiZTBmOWFiYzA5ZDUxMmUzZA",
+        "title": "boAt Rockerz 255 Pro Wireless Neckband Earphone with Up to 10 Hours Playback, Midnightblue",
+        "brand": "BOAT",
+        "rating": 4.9,
+        "offerprice": 899.0,
+        "mrp": 3499.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 7,
+        "image": "https://www.reliancedigital.in/medias/Realme-Q2-ANC-TWS-Buds-492338704-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w3NTc5fGltYWdlL2pwZWd8aW1hZ2VzL2hlNC9oNDAvOTYxMDczODIzNzQ3MC5qcGd8YjAwZGNkN2UwYWI5YzQ1NGU2Y2FjMTVhOWZkYzUxODMwNzFiYTg3N2UxOTY3ZDZjNWM0YzkyMDgxNGJhMGU4MQ",
+        "title": "Realme Buds Q2 True wireless Earphone with Active Noise Cancellation, 28 hours Total Playback, Active Black",
+        "brand": "REALME",
+        "rating": 4.7,
+        "offerprice": 1699.0,
+        "mrp": 3499.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 8,
+        "image": "https://www.reliancedigital.in/medias/boAt-Airdopes148-TrueWireless-492579512-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxODQxNHxpbWFnZS9qcGVnfGltYWdlcy9oODMvaDIwLzk4MTY0MDE1MTA0MzAuanBnfDA2YzYxZGIxZmViZGE4MmY4ZTI0MTI0MmNhMDY1YzYzYTI1NjQzZjY4YWMyY2ZkNjNhYWYzZTUwNTE0NmE1Yzk",
+        "title": "boAt Airdopes 148 True Wireless Airdopes, Active Black",
+        "brand": "BOAT",
+        "rating": 4.3,
+        "offerprice": 1299.0,
+        "mrp": 4499.0,
+        "category": "Earphone"
+    },
+    {
+        "id": 9,
+        "image": "https://www.reliancedigital.in/medias/OnePlus-Bullets-Z2-Wireless-Earphone-492796790-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MTk1fGltYWdlL2pwZWd8aW1hZ2VzL2hjNi9oMDkvOTgzODE4MDA0MDczNC5qcGd8NWRkMjEyZjQxMTBkYzliZmY1ZTA0NTIwMmQ3MWMxZjgyMDE3ZjdiMjVmMmFjNGY1OTg4NWNjNDU0NTY0MzdlOA",
+        "title": "OnePlus Bullets Z2 Wireless Neckband Earphone with AI Noise Cancellation, Magico Black",
+        "brand": "ONE PLUS",
+        "rating": 4.5,
+        "offerprice": 1999.0,
+        "mrp": 4500.0,
+        "category": "Earphone"
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Apple-iPhone-14-Plus-Mobile-Phone-493177767-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDEwNHxpbWFnZS9qcGVnfGltYWdlcy9oOWIvaGFmLzk4OTA1NDE5NjEyNDYuanBnfGEwNTZiM2Y3MWY1YmFmOTU3MDI2MjUxM2E0NTI0NTIwMzFhMTE5Y2NjNzQ2MWY1MWI5ZjVmN2JjMmRjZjY3YTU",
+        title: "Apple iPhone 14 Plus 128 GB, Blue",
+        brand: "APPLE",
+        rating: 4.9,
+        offerprice: 89990.0,
+        mrp: 95999.0,
+        category: "Mobile",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Apple-iPhone-14-Mobile-Phone-493177759-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMzEyMHxpbWFnZS9qcGVnfGltYWdlcy9oY2EvaGRhLzk4OTA1MjA5MjQxOTAuanBnfDVjZGQ3NzdkZmU4NmI4MzBjNmM3ODJjMTI2MWFiMTcxM2ViNzQ3ODE2YzA1MjIwZTliZGNiZmIxYWVmYWQyYzY",
+        title: "Apple iPhone 14 Plus 128 GB, Midnight",
+        brand: "APPLE",
+        rating: 4.8,
+        offerprice: 109990.0,
+        mrp: 125999.0,
+        category: "Mobile",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Apple-iPhone-14-Plus-Mobile-Phone-493177767-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDEwNHxpbWFnZS9qcGVnfGltYWdlcy9oOWIvaGFmLzk4OTA1NDE5NjEyNDYuanBnfGEwNTZiM2Y3MWY1YmFmOTU3MDI2MjUxM2E0NTI0NTIwMzFhMTE5Y2NjNzQ2MWY1MWI5ZjVmN2JjMmRjZjY3YTU",
+        title: "Apple iPhone 14 Plus 128 GB, Blue",
+        brand: "APPLE",
+        rating: 4.9,
+        offerprice: 109990.0,
+        mrp: 125999.0,
+        category: "Mobile",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Apple-iPhone-14-Plus-Mobile-Phone-493177767-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNDEwNHxpbWFnZS9qcGVnfGltYWdlcy9oOWIvaGFmLzk4OTA1NDE5NjEyNDYuanBnfGEwNTZiM2Y3MWY1YmFmOTU3MDI2MjUxM2E0NTI0NTIwMzFhMTE5Y2NjNzQ2MWY1MWI5ZjVmN2JjMmRjZjY3YTU",
+        title: "Apple iPhone 14 Plus 128 GB, Blue",
+        brand: "APPLE",
+        rating: 5.0,
+        offerprice: 119990.0,
+        mrp: 123999.0,
+        category: "Mobile",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Bajaj-740076-INDUCTIONCOOKTOPS-492573223-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMjMyNXxpbWFnZS9qcGVnfGltYWdlcy9oY2IvaDg2Lzk3MzQ4NjQwNzY4MzAuanBnfDQ5YjU1ZmNjZWU4ZjIwNzMxMjM5ZWM5MTY5YWMwYWJlZDlhZGI5M2IwZThkYjAwYzdjNGU2Y2VlZjljMzJlZTI",
+        title:
+            "Bajaj Majesty Slim 2100-Watt Induction Cooktop with 9 Auto Cook Menus (Black)",
+        brand: "BAJAJ",
+        rating: 4.3,
+        offerprice: 3799.0,
+        mrp: 6100.0,
+        category: "Kitchen",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Vega-VHTH19-Epilators-491902978-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNTI0NXxpbWFnZS9qcGVnfGltYWdlcy9oZGMvaDQyLzk0MTM1MDAzNzA5NzQuanBnfGIwZWRlMDQ4NWNmY2Y0NTZjODZiZTIwMmNiYWZmYjUxOTY0OGQzNTQwZjRiMTVlMDU3ZDFkYjI4YmEwM2E0MGI",
+        title: "Vega T3 VHTH-19 Beard Hair Trimmer",
+        brand: "VEGA",
+        rating: 4.6,
+        offerprice: 1025.0,
+        mrp: 1299.0,
+        category: "Dailyuse",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Philips-HL7707-00-Mixer-Grinder-491472892-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzNTI3MXxpbWFnZS9qcGVnfGltYWdlcy9oMmEvaDFjLzkyOTI2ODc1MDc0ODYuanBnfDkwMmViNmViM2U1N2ExYjJmZjhiNDg0MzBhMWE1NzAwYWE1MGRkNWNkM2JlYTI4NWE2NWJkOTliYjMzZDMxNmM",
+        title:
+            "Phillips Avance Collection HL7707/00 750-Watt Mixer Grinder with 4 Jars, PowerChop technology (Black)",
+        brand: "PHILLIPS",
+        rating: 4.5,
+        offerprice: 9199.0,
+        mrp: 11995.0,
+        category: "Kitchen",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Philips-GC523-60-Specialty-Appliances-491433174-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w1NzIwfGltYWdlL2pwZWd8aW1hZ2VzL2g5ZC9oMjUvOTIxNzA2Mzg0NTkxOC5qcGd8YTYwMDdmNjVkZGZlMTMyOTJhNjU1NTI4NDIyNjA2ZmVhMDYxN2Y0NTQzMGJhN2I5Yjg1YzdlY2E4NDJhN2YzNQ",
+        title: "Philips EasyTouch Plus GC523/60 Garment Steamer",
+        brand: "PHILLIPS",
+        rating: 4.7,
+        offerprice: 7999.0,
+        mrp: 9995.0,
+        category: "Dailyuse",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/pTron-140318176-Smart-Watch-492850976-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzOTM4MHxpbWFnZS9qcGVnfGltYWdlcy9oNGYvaGJiLzk4OTU1OTAyOTc2MzAuanBnfDFkODRmMGE2ZWMyNTEzY2I5OTI2YzhiZTJkM2I0M2E0Y2NhMWNiNjRmNjA4MGNmMzE4YTZiOTg1NjZlNmRhMzM",
+        title:
+            "pTron Pulsefit F21 Plus Smart Watch with 4.31 cm (1.7 Inch) Full Touch Color Display, Black",
+        brand: "pTRON",
+        rating: 4.8,
+        offerprice: 999.0,
+        mrp: 1299.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Fire-Boltt-BSW007-Smart-Watch-492850759-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNTkwMnxpbWFnZS9qcGVnfGltYWdlcy9oMmIvaDBlLzk4NTk3ODYyNzY4OTQuanBnfDdiMTBhZThkYWQ1ZDc5N2Q0NTk5MDJmYjhiYTFmNGU3MTRiMzYwN2M2YzVlMzNhNTI0N2UzMGNkZGZiY2QyMmI",
+        title:
+            "Fire-Boltt Ninja Smart Watch with Touch to Wake Feature, IP67 Water Resistant (Black)",
+        brand: "BOAT",
+        rating: 4.9,
+        offerprice: 1699.0,
+        mrp: 4999.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Hammer-PULSE-3-0-Smart-Watch-492849894-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNjcyMXxpbWFnZS9qcGVnfGltYWdlcy9oMDcvaGU1Lzk4MTQ3MTk5MjIyMDYuanBnfDU4NmIxM2U0OWY3NGRmYjI3ZTA0NDQwN2EzN2ZmMDgwYjllYTQ4ZjU5OGU4MmUwYzVlNDhmN2I0OGNiZTgzYWU",
+        title:
+            "Hammer Pulse 3.0 Bluetooth Calling Smartwatch with Honeycomb Design (Black)",
+        brand: "HAMMER",
+        rating: 4.9,
+        offerprice: 1799.0,
+        mrp: 9999.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/boAt-Watch-Flash-RTL-Smart-Watches-492574195-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2MDAwN3xpbWFnZS9qcGVnfGltYWdlcy9oNDMvaDA3Lzk3NTU3Nzg1NDc3NDIuanBnfDQxZGE4MzYwMzI2NjZiZjJkYWQyM2VmOGNiZTc5OWE0YmU0NTM3ODk3ZjgwMGZlYmNjMThhMTRjZmM5NDMyN2Y",
+        title:
+            "boAt Flash RTL 3.3 cm (1.29 inch) Smart Watch with Customizable Watch Faces, Lightning Black",
+        brand: "BOAT",
+        rating: 4.5,
+        offerprice: 2499.0,
+        mrp: 6900.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Noise-Colorfit-Icon-Plus-Smart-Watch-492850516-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wxNTk5M3xpbWFnZS9qcGVnfGltYWdlcy9oMzEvaGYyLzk4Nzc2Mzc3NTkwMDYuanBnfDNiMDIzZWUxNjVkZTFkYWQzOTFiYmEyYmRmM2UxMjg1MGY1MWRmNTA1OGE2NTllNzgyMzg1MTVjM2E5NWFmZTU",
+        title: "Noise Colorfit Icon Plus Smart Watch, Jet Black",
+        brand: "NOISE",
+        rating: 4.8,
+        offerprice: 2699.0,
+        mrp: 5900.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/pTron-PULSEFIT-P261-Smart-Watch-493177969-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMDUzNnxpbWFnZS9qcGVnfGltYWdlcy9oMGEvaGIzLzk4OTQ2NzAyMzc3MjYuanBnfDJjMTc3ODdhNzNiMTU2YWU2MDM4ZTRlYTQwYTljMDk1MWRmMjgwMWFkNzI4MmM5NTU2MzA3NjUzNjhkYmZkYWI",
+        title:
+            "pTron Pulsefit P261 Bluetooth Calling Smartwatch with IP68 Waterproof, Black",
+        brand: "pTRON",
+        rating: 4.2,
+        offerprice: 1499.0,
+        mrp: 5900.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/pTron-PULSEFIT-P261-Smart-Watch-493177971-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wzMTY5MHxpbWFnZS9qcGVnfGltYWdlcy9oNWUvaGVjLzk4OTQ2NzEyMjA3NjYuanBnfGU4Zjk4YzFiMWE0MTZhYTk4MTEzYWIzYjJmNDViMGM5ZDY0MmY1ZWM1OWE0NWIyNzc1OGFmMjUwN2EyZjc3YjU",
+        title:
+            "pTron Pulsefit P261 Bluetooth Calling Smartwatch with IP68 Waterproof, Blue",
+        brand: "pTRON",
+        rating: 4.4,
+        offerprice: 1499.0,
+        mrp: 2300.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Fire-Boltt-BSW007-Smart-Watch-492850760-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyMzEwMXxpbWFnZS9qcGVnfGltYWdlcy9oMjMvaDY2Lzk4NTk3OTkwNTY0MTQuanBnfDEzNzY0MGE3MzkzYjUwMmE4OWNlZThmMjRmMDg3NWI3OWUzZjk3ODAzYzBhOTgzNTBlNjc1M2E2MDFlNjU0ZWI",
+        title:
+            "Fire-Boltt Ninja Smart Watch with Touch to Wake Feature, IP67 Water Resistant (Grey)",
+        brand: "BOLT",
+        rating: 4.9,
+        offerprice: 1699.0,
+        mrp: 2499.0,
+        category: "Watch",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Kodak-32HDX900S-Televisions-491431274-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w4MjQ1MHxpbWFnZS9qcGVnfGltYWdlcy9oYjMvaGIxLzkzMjQ0NzM5NzQ4MTQuanBnfGE2YmQwOWViYzM4ZDQ2ZTczMjE3MWNkZTU4ZjY0YWU4YmUzMjQ2NzM5MGM3Y2NjZjBjNzk4MjdmZjEzNTJlZGE",
+        title: "Kodak 80 cm (32 inch) HD Ready LED TV, 32HDX900s",
+        brand: "KODAK",
+        rating: 4.9,
+        offerprice: 8900.0,
+        mrp: 9989.0,
+        category: "Telivision",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Kodak-43CA2022-Television-492166175-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3w2ODUwMnxpbWFnZS9qcGVnfGltYWdlcy9oOGIvaDEwLzk1MTIxMDYyMjk3OTAuanBnfDgwNTMwNDExYzI5MjkyNDJkZmZlMTI0ODAwYmM4OGNlZTZiM2U3MWQzM2FhNWI1ZDJmOGE5ZDJhYzg3ZjkxZGM",
+        title:
+            "Kodak 108 cm (43 inch) Ultra HD (4K) Premium Android TV with Dolby Digital Plus and DTS TruSurround, CA Series 43CA2022",
+        brand: "KODAK",
+        rating: 4.4,
+        offerprice: 20999.0,
+        mrp: 25000.0,
+        category: "Telivision",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Kodak-50CAPRO5066-Televisions-492579288-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyOTc1M3xpbWFnZS9qcGVnfGltYWdlcy9oYjYvaDhmLzk4OTQ3MjA3NjU5ODIuanBnfGYxYzA1M2Y2MmZiZjIxZGM3MmU0YTQ2NTBjNTg1ZGE0M2Y2NGM3NGZjYjNhZTUzZGMxYjhiMzY1MjVlZGUxMmE",
+        title:
+            "Kodak 126 cm (50 inch) Ultra HD (4K) Smart LED TV, CAPRO Series 50CAPRO5066",
+        brand: "KODAK",
+        rating: 4.9,
+        offerprice: 24999.0,
+        mrp: 35654.0,
+        category: "Telivision",
+    },
+    {
+        image:
+            "https://www.reliancedigital.in/medias/Kodak-40FHDX7XPRO-Televisions-492166142-i-1-1200Wx1200H-300Wx300H?context=bWFzdGVyfGltYWdlc3wyNTgwM3xpbWFnZS9qcGVnfGltYWdlcy9oMmMvaDhjLzk4OTQ2ODg5ODEwMjIuanBnfDM0YjhiY2Q5ZTVkYThlZGM5MzRlNTZlOWM1YWU0Mjk2OGJlYWE2YTQwMGQwNjRkZGFjYWQ2NjU5N2JmZmY0OGQ",
+        title:
+            "Kodak 102 cm (40 inch) Full HD LED Smart TV, 7XPRO Series 40FHDX7XPRO, Built-in Chromecast, Quad Core Processor",
+        brand: "KODAK",
+        rating: 5.0,
+        offerprice: 15999.0,
+        mrp: 24000.0,
+        category: "Telivision",
+    },
+]
+
+
+
+const all_iteams = document.getElementById("audioContainer");
+const filterBy = document.getElementById("filter");
+const FilterBy = document.getElementById("Filter");
+let LSdata = JSON.parse(localStorage.getItem("degitalProduct")) || []
+
+
+Display_product(all_produt)
+
+// filter part
+
+filterBy.addEventListener("change", function () {
+    if (filterBy.value == "") {
+        Display_product(all_produt)
+    } else {
+        let filteredData = all_produt.filter(function (element) {
+            return element.category == filterBy.value
+        });
+        Display_product(filteredData)
+    }
+
+})
+
+// product display...//
+
+
+function Display_product(data) {
+    audioContainer.innerHTML = "";
+    // for (i = 0; i < all_produt.length; i++) 
+    data.forEach((Element) => {
+        const card = document.createElement("div");
+
+        const image = document.createElement("img")
+        image.src = Element.image
+
+        const title = document.createElement("p");
+        title.innerText = Element.title;
+
+
+        const brand = document.createElement("h6");
+        brand.innerText = Element.brand;
+
+        const rating = document.createElement("h6")
+        rating.innerHTML = "Rating - " + Element.rating + " ⭐⭐";
+
+        const offerprice = document.createElement("h5")
+        offerprice.innerHTML = "Deal Price - ₹" + Element.offerprice;
+
+        let mrp = document.createElement("h6");
+        mrp.innerHTML = "M.R.P. - ₹" + Element.mrp;
+
+        let discount = document.createElement("h6");
+        discount.innerHTML = "You Save - ₹" + Number(Element.mrp - Element.offerprice);
+
+        let category = document.createElement("h6");
+        category.innerHTML = "Category - " + Element.category;
+
+        let addtoCart = document.createElement("button");
+        addtoCart.innerText = "cart"
+
+        addtoCart.addEventListener("click", function () {
+            LSdata.push(Element)
+
+            localStorage.setItem("degitalProduct", JSON.stringify(LSdata));
+            alert("Product Added")
+        })
+
+
+
+        card.append(image, title, brand, rating, offerprice, discount, category, addtoCart);
+        all_iteams.append(card);
+
+
+    })
+}
+
+
+  //   search bar..
+
